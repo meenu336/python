2c7d9853056cd1ccf85d7672b628bf08f6983b83
Meenu <annameenu00@gmail.com>
2022-12-18 09:25:58 +0530

    second commit

---
+++ calculator.py
@@ -0,0 +1,10 @@
+x=int(input("Enter any number:"))
+y=int(input("Enter any number:"))
+z=x+y
+u=x-y
+v=x*y
+w=x/y
+print("Addition of two numbers:",z)
+print("Substration of two numbers:",u)
+print("Multiplication of two numbers:",v)
+print("Division of two numbers:",w)
